b1bdd1ad0db71e2670ee6625e949bbf7324e4bab
piotryfon <31383071+piotryfon@users.noreply.github.com>
2022-04-02 18:57:05 +0200

    Add project files.

---
+++ ConsoleAppEmployeeDb/Controllers/EmployeesController.cs
@@ -0,0 +1,43 @@
+﻿using ConsoleAppEmployeeDb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ConsoleAppEmployeeDb.Controllers
+{
+    internal class EmployeesController
+    {
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EmployeesDb;
+                        Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;
+                        ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        
+        public List<Employee> GetEmployeesFromDatabase()
+        {
+            var result = new List<Employee>();
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            var query = $"select * from Employees";
+            using (SqlCommand readCommand = new SqlCommand(query, connection))
+            {
+                SqlDataReader reader = readCommand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Employee employee = new Employee
+                    {
+                        Id = (int)reader["Id"],
+                        FirstName = (string)reader["firstname"],
+                        LastName= (string)reader["lastname"],
+                        Salary = (int)reader["salary"],
+                    };
+                    result.Add(employee);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ConsoleAppEmployeeDb/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleAppEmployeeDb.Controllers;
+using System;
+
+namespace ConsoleAppEmployeeDb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeesController employeesController = new EmployeesController();
+            var result = employeesController.GetEmployeesFromDatabase();
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.FirstName} {item.LastName} - {item.Salary} zł");
+            }
+        }
+
+     }
+} 
